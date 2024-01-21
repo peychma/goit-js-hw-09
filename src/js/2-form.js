fc7b2f2fe14a1a6160884b05efe2d8aa2c4c22a4
peychma <mariapei4eva@gmail.com>
2024-01-21 21:38:31 +0200

    .

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,28 +1,35 @@
 const feedbackForm = document.querySelector(".feedback-form");
-feedbackForm.addEventListener("input", function (event) {
-  if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+document.addEventListener("DOMContentLoaded", function () {
+  const storedData = localStorage.getItem("feedback-form-state");
+  if (storedData) {
     const { email, message } = feedbackForm.elements;
-    const feedbackState = {
-      email: email.value.trim(),
-      message: message.value.trim(),
-    };
-    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackState));
+    email.value = parsedData.email;
+    message.value = parsedData.message;
   }
+});
+feedbackForm.addEventListener("input", function (event) {
+  const { email, message } = feedbackForm.elements;
+  const feedbackState = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem("feedback-form-state", JSON.stringify(feedbackState));
 });
 feedbackForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const { email, message } = feedbackForm.elements;
-  if (!email.value.trim() || !message.value.trim()) {
+  const trimmedEmail = email.value.trim();
+  const trimmedMessage = message.value.trim();
+  if (!trimmedEmail || !trimmedMessage) {
     alert("Please fill in two fields before submitting.");
     return;
   }
   const formData = {
-    email: email.value.trim(),
-    message: message.value.trim(),
+    email: trimmedEmail,
+    message: trimmedMessage,
   };
   console.log(formData);
   localStorage.removeItem("feedback-form-state");
   email.value = "";
   message.value = "";
-});
-
+})
