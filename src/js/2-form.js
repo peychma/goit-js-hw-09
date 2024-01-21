d2b936966e2f14ac58dc5c17b13ab21afd8e39f2
peychma <mariapei4eva@gmail.com>
2024-01-21 21:44:42 +0200

    .

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,12 +1,16 @@
 const feedbackForm = document.querySelector(".feedback-form");
+
 document.addEventListener("DOMContentLoaded", function () {
   const storedData = localStorage.getItem("feedback-form-state");
+
   if (storedData) {
     const { email, message } = feedbackForm.elements;
+    const parsedData = JSON.parse(storedData);
     email.value = parsedData.email;
     message.value = parsedData.message;
   }
 });
+
 feedbackForm.addEventListener("input", function (event) {
   const { email, message } = feedbackForm.elements;
   const feedbackState = {
@@ -15,21 +19,26 @@ feedbackForm.addEventListener("input", function (event) {
   };
   localStorage.setItem("feedback-form-state", JSON.stringify(feedbackState));
 });
+
 feedbackForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const { email, message } = feedbackForm.elements;
   const trimmedEmail = email.value.trim();
   const trimmedMessage = message.value.trim();
+
   if (!trimmedEmail || !trimmedMessage) {
     alert("Please fill in two fields before submitting.");
     return;
   }
+
   const formData = {
     email: trimmedEmail,
     message: trimmedMessage,
   };
+
   console.log(formData);
   localStorage.removeItem("feedback-form-state");
+
   email.value = "";
   message.value = "";
-})
+});
